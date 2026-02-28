827a6a5913de8bb8c232ffe7e50a2f0354733ad0
yasirbilir06 <yasiryenihasan@gmail.com>
2026-02-28 20:12:03 +0100

    Initial Commit

---
+++ DeutchFlash/DeutchFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeutchFlashApp.swift
+//  DeutchFlash
+//
+//  Created by Yasir Bilir on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DeutchFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
